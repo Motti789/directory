de4c5343e596878cdad3fd5d8354ebba7075e476
Motti789 <mchillywilly@gmail.com>
2022-11-24 10:41:49 -0500

    created a from and imported it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Table from './Table'
+import Form from './Form';
 
 class App extends Component {
   state = {
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div className="Container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        < Form />
       </div>
     )
   }
